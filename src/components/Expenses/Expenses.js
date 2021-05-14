bc55b5c6705ddb2a5f511aa4da72bcc03e3ff191
Arnab <arnab@felicity.care>
2021-05-15 00:39:54 +0530

    completed the expense calculator app

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,32 +1,31 @@
-import ExpenseItem from './ExpenseItem';
+import React, {useState} from 'react';
 import './Expenses.css';
 import Card from '../UI/Card';
+import ExpenseFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  }
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
  return (
+   <div>
      <Card className="expenses">
-         <h2>Let's get started!</h2>
-         <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
+         <ExpenseFilter 
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler} 
+          />
+          <ExpensesChart expenses={filteredExpenses}/>
+          <ExpensesList items={filteredExpenses} />
      </Card>
+     </div>
  );
 }
 
